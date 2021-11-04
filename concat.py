074ca9c28c8c8000aefb90ffe1fbb3d87435a582
Oba Daisuke <oba.daisuke.278@s.kyushu-u.ac.jp>
2021-11-04 11:00:05 +0900

    change names

--- concat.py
+++ concat.py
@@ -13,7 +13,8 @@ from model.dda import *
 from utils.dataload import data_generator
 from utils.draw_graphs import draw_graph, draw_hist
 from utils.write_gspread import *
-from utils.alphavis import alpha_number_line
+from utils.alphavis import alpha_pca
+from utils.feature_pca import feature_pca
 
 parser = argparse.ArgumentParser(description='Sequence Modeling')
 parser.add_argument('--batch_size', type=int, default=256, metavar='N',
@@ -38,6 +39,8 @@ parser.add_argument('--da5', default='timeWarp',
                     help='Data Augmentation 5 (default: timeWarp)')
 parser.add_argument('--shareWeights', action='store_true',
                     help='share network weights')
+parser.add_argument('--consis_lambda', type=float, default=1.0,
+                    help='weights for consistency loss')
 parser.add_argument('--server_id', type=int, default=0,
                     help='when run on local, set it to 0.')
 parser.add_argument('--limit_num', type=int, default=300,
@@ -73,26 +76,33 @@ z_size = 512 # f's size
 input_channels = 1
 seq_length = int(input_length / input_channels) 
 epochs = np.ceil(args.iterations * (batch_size / NumOfTrain)).astype(int)
-print(batch_size, epochs)
 data_len_after_cnn = int((((((input_length-2)/2)-2)/2)-2)/2)
 
 steps = 0
-da1, da2, da3, da4, da5 = args.da1, args.da2, "NA", "NA", "NA"
+da1, da2, da3, da4, da5 = args.da1, args.da2, args.da3, args.da4, args.da5
 
 print(args, 'epochs:{}'.format(epochs))
 train_loader, test_loader, _ = data_generator(data_name, batch_size, da1, da2, da3, da4, da5, dataset_path)
 
 ts_encoder1, ts_encoder2 = TSEncoder(data_len_after_cnn, z_size), TSEncoder(data_len_after_cnn, z_size)
-classifier = Classifier(n_classes, z_size*2) # for concat
+ts_encoder3, ts_encoder4 = TSEncoder(data_len_after_cnn, z_size), TSEncoder(data_len_after_cnn, z_size)
+ts_encoder5 = TSEncoder(data_len_after_cnn, z_size)
+classifier = Classifier(n_classes, z_size*5) # for concat
 
 ts_encoder1.cuda(gpu_id)
 ts_encoder2.cuda(gpu_id)
+ts_encoder3.cuda(gpu_id)
+ts_encoder4.cuda(gpu_id)
+ts_encoder5.cuda(gpu_id)
 classifier.cuda(gpu_id)
     
 lr = args.lr
 optimizer = optim.Adam(
-    [{'params': ts_encoder1.parameters()}, 
-    {'params': ts_encoder2.parameters()}, 
+    [{'params': ts_encoder1.parameters()},
+    {'params': ts_encoder2.parameters()},
+    {'params': ts_encoder3.parameters()},
+    {'params': ts_encoder4.parameters()},
+    {'params': ts_encoder5.parameters()},
     {'params': classifier.parameters()},
     ], lr=lr, betas=(0.5, 0.999))
 
@@ -104,17 +114,25 @@ def train(ep):
     correct = 0
     ts_encoder1.train()
     ts_encoder2.train()
+    ts_encoder3.train()
+    ts_encoder4.train()
+    ts_encoder5.train()
     classifier.train()
     
-    for da1_data, da2_data, _, _, _, target, _ in train_loader:
+    for da1_data, da2_data, da3_data, da4_data, da5_data, target, _ in train_loader:
         da1_data, da2_data, target = da1_data.cuda(gpu_id).to(dtype=torch.float), da2_data.cuda(gpu_id).to(dtype=torch.float), target.cuda(gpu_id)
-        da1_data = da1_data.view(-1, input_channels, seq_length)
-        da2_data = da2_data.view(-1, input_channels, seq_length)
-        da1_data, da2_data, target = Variable(da1_data), Variable(da2_data), Variable(target)
+        da3_data, da4_data, da5_data = da3_data.cuda(gpu_id).to(dtype=torch.float), da4_data.cuda(gpu_id).to(dtype=torch.float), da5_data.cuda(gpu_id).to(dtype=torch.float)
         
-        z1, z2 = ts_encoder1(da1_data), ts_encoder2(da2_data)
-        z = torch.cat((z1,z2), 1)
+        da1_data, da2_data = da1_data.view(-1, input_channels, seq_length), da2_data.view(-1, input_channels, seq_length)
+        da3_data, da4_data, da5_data = da3_data.view(-1, input_channels, seq_length), da4_data.view(-1, input_channels, seq_length), da5_data.view(-1, input_channels, seq_length)
+        
+        da1_data, da2_data, target = Variable(da1_data), Variable(da2_data), Variable(target)
+        da3_data, da4_data, da5_data = Variable(da3_data), Variable(da4_data), Variable(da5_data)
+        
+        z1, z2, z3, z4, z5 = ts_encoder1(da1_data), ts_encoder2(da2_data), ts_encoder3(da3_data), ts_encoder4(da4_data), ts_encoder5(da5_data)
+        z = torch.cat((z1,z2,z3,z4,z5), 1)
         y = classifier(z)
+        
         loss = CE_loss(y, target)
         
         optimizer.zero_grad()
@@ -125,35 +143,52 @@ def train(ep):
         
         train_loss += loss
     train_loss /= len(train_loader.dataset)
-    #print('Train set: Average loss: {:.8f}, Accuracy: {:>4}/{:<4} ({:>3.0f}%)'.format(
+    #print('     Train set: Average loss: {:.8f}, Accuracy: {:>4}/{:<4} ({:>3.0f}%)'.format(
     #    train_loss, correct, len(train_loader.dataset), 100. * correct / len(train_loader.dataset)))
             
     return train_loss, correct/len(train_loader.dataset)
 
-def test(epoch, showParams=False, da1name=None, da2name=None):
+def test(epoch, feature_img_path=None):
     global now
     test_loss = 0.
     correct = 0
     ts_encoder1.eval()
     ts_encoder2.eval()
+    ts_encoder3.eval()
+    ts_encoder4.eval()
+    ts_encoder5.eval()
     classifier.eval()
 
     with torch.no_grad():
-        for da1_data, da2_data, _, _, _, target, _ in test_loader:
+        for da1_data, da2_data, da3_data, da4_data, da5_data, target, _ in test_loader:
             da1_data, da2_data, target = da1_data.cuda(gpu_id).to(dtype=torch.float), da2_data.cuda(gpu_id).to(dtype=torch.float), target.cuda(gpu_id)
+            da3_data, da4_data, da5_data = da3_data.cuda(gpu_id).to(dtype=torch.float), da4_data.cuda(gpu_id).to(dtype=torch.float), da5_data.cuda(gpu_id).to(dtype=torch.float)
             da1_data = da1_data.view(-1, input_channels, seq_length)
             da2_data = da2_data.view(-1, input_channels, seq_length)
+            da3_data = da3_data.view(-1, input_channels, seq_length)
+            da4_data = da4_data.view(-1, input_channels, seq_length)
+            da5_data = da5_data.view(-1, input_channels, seq_length)
             da1_data, da2_data, target = Variable(da1_data), Variable(da2_data), Variable(target)
+            da3_data, da4_data, da5_data = Variable(da3_data), Variable(da4_data), Variable(da5_data)
             
-            z1, z2 = ts_encoder1(da1_data), ts_encoder2(da2_data)
-            z = torch.cat((z1,z2), 1)
-            target_list = target.to('cpu').detach().numpy() if not 'target_list' in locals() else np.concatenate([target_list, target.to('cpu').detach().numpy()])
+            z1, z2, z3, z4, z5 = ts_encoder1(da1_data), ts_encoder2(da2_data), ts_encoder3(da3_data), ts_encoder4(da4_data), ts_encoder5(da5_data)
+            
+            z1_list = z1 if not 'z1_list' in locals() else torch.cat([z1_list, z1])
+            z2_list = z2 if not 'z2_list' in locals() else torch.cat([z2_list, z2])
+            z3_list = z3 if not 'z3_list' in locals() else torch.cat([z3_list, z3])
+            z4_list = z4 if not 'z4_list' in locals() else torch.cat([z4_list, z4])
+            z5_list = z5 if not 'z5_list' in locals() else torch.cat([z5_list, z5])
+            
+            target_list = target.to('cpu').detach().numpy() if not 'target_list' in locals() else np.concatenate([target_list, target.to('cpu').detach().numpy()])            
 
+            z = torch.cat((z1,z2,z3,z4,z5), 1)
             y = classifier(z)
+            
             _, predict = torch.max(y.data, 1)
             test_correct = (predict == target).sum().item()
 
             loss = CE_loss(y, target)
+            
             pred = y.data.max(1, keepdim=True)[1]
             correct += pred.eq(target.data.view_as(pred)).cpu().sum()
             
@@ -162,41 +197,76 @@ def test(epoch, showParams=False, da1name=None, da2name=None):
 
         pred_list = np.array([item for l in pred_list for item in l ])
         test_loss /= len(test_loader.dataset)
-        print(' Epoch:{} Test set: Average loss: {:.8f}, Accuracy: {:>4}/{:<4} ({:>3.0f}%)'.format(
-            epoch, test_loss, correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)))
-
-        return test_loss, correct / len(test_loader.dataset)
-
+        print('      Test set: Average loss: {:.8f}, Accuracy: {:>4}/{:<4} ({:>3.0f}%)'.format(
+            test_loss, correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)))
+        
+        random.seed(42) # must
+        idx_list = np.arange(len(target_list))
+        if len(target_list)>limit_num:
+            idx_list = random.sample(list(idx_list), limit_num)
+            draw_target = itemgetter(idx_list)(target_list)
+            draw_pred = itemgetter(idx_list)(pred_list)
+            draw_z1 = itemgetter(idx_list)(z1_list[:])
+            draw_z2 = itemgetter(idx_list)(z2_list[:])
+            draw_z3 = itemgetter(idx_list)(z3_list[:])
+            draw_z4 = itemgetter(idx_list)(z4_list[:])
+            draw_z5 = itemgetter(idx_list)(z5_list[:])
+        else:
+            draw_target = target_list
+            draw_pred = pred_list
+            draw_z1 = z1_list[:]
+            draw_z2 = z2_list[:]
+            draw_z3 = z3_list[:]
+            draw_z4 = z4_list[:]
+            draw_z5 = z5_list[:]
+        
+        # only when using saved model
+        feature_pca("TSNE", data_name, da1, draw_z1, -1, da2, draw_z2, -1, da3, draw_z3, -1, da4, draw_z4, -1, da5, draw_z5, -1, epoch, 100*correct/len(test_loader.dataset), './', -1)
+        
+        torch.cuda.empty_cache()
+        
+        return test_loss, correct / len(test_loader.dataset), -1, -1, -1, -1, -1
 
 def test_model():
-    #model_path = glob.glob('/home/daisuke/Downloads/last_test_accuracy/2en-shared-with-model/*{}*{}*{}*.pth'.format(data_name, da1, da2))
-    model_path = glob.glob('/home/daisuke/Documents/CloudStation/lab/Codes/DynamicDA/data/*{}*{}*{}*.pth'.format(data_name, da1, da2))
-    #da1name, da2name, = model_path[0].split("_")[2].split("-vs-")[0], model_path[0].split("_")[2].split("-vs-")[1]
-    print(model_path)
-    model.load_state_dict(torch.load(model_path[0], map_location='cuda:0'))
-    test(1, da1name=da1, da2name=da2, report=False) # pseudo epoch
+    base_path = '/home/daisuke/Documents/CloudStation/lab/Codes/DDA-matsuo-based/Matsuo-5DA_{}_identity-jitter-magnitudeWarp-windowWarp-timeWarp_{}_{}'.format(data_name, args.consis_lambda, epochs)
+    #base_path = '/home/daisuke/Documents/CloudStation/lab/Codes/DDA-matsuo-based/Matsuo-5DA_FordA_identity-jitter-magnitudeWarp-windowWarp-timeWarp_1.0_356'
+    #base_path = '/home/daisuke/Documents/CloudStation/lab/Codes/DDA-matsuo-based/Matsuo-5DA_PhalangesOutlinesCorrect_identity-jitter-magnitudeWarp-windowWarp-timeWarp_1.0_712'
+    #base_path = '/home/daisuke/Documents/CloudStation/lab/Codes/DDA-matsuo-based/Matsuo-5DA_StarLightCurves_identity-jitter-magnitudeWarp-windowWarp-timeWarp_1.0_1280'
+    #base_path = '/home/daisuke/Documents/CloudStation/lab/Codes/DDA-matsuo-based/Matsuo-5DA_TwoPatterns_identity-jitter-magnitudeWarp-windowWarp-timeWarp_1.0_1280'
+    #base_path = '/home/daisuke/Documents/CloudStation/lab/Codes/DDA-matsuo-based/Matsuo-5DA_Wafer_identity-jitter-magnitudeWarp-windowWarp-timeWarp_1.0_1280'
+    ts_encoder1.load_state_dict(torch.load(base_path+'/ts_encoder1.pth', map_location='cuda:0'))
+    ts_encoder2.load_state_dict(torch.load(base_path+'/ts_encoder2.pth', map_location='cuda:0'))
+    ts_encoder3.load_state_dict(torch.load(base_path+'/ts_encoder3.pth', map_location='cuda:0'))
+    ts_encoder4.load_state_dict(torch.load(base_path+'/ts_encoder4.pth', map_location='cuda:0'))
+    ts_encoder5.load_state_dict(torch.load(base_path+'/ts_encoder5.pth', map_location='cuda:0'))
+    gating5.load_state_dict(torch.load(base_path+'/gating5.pth', map_location='cuda:0'))
+    classifier.load_state_dict(torch.load(base_path+'/classifier.pth', map_location='cuda:0'))
+    test(epochs, base_path) # pseudo epoch.
     exit(0)
 
 if __name__ == "__main__":
     #test_model()
-    best_loss, best_acc = 10e9, 0.
+    best_loss, best_acc = 10e5, 0.
     for epoch in range(1, epochs+1):
+        print('Epoch:{}/{}'.format(epoch, epochs))
         train_loss, train_acc = train(epoch)
-        if epoch%10==0 or epoch==1:
-            test_loss, test_acc = test(epoch, showParams=False, da1name=da1, da2name=da2)
-        #else:
-            #test_loss, test_acc = test(epoch, showParams=False, da1name=da1, da2name=da2)
+        if epoch%25==0 or epoch==epochs or epoch==1:
+            test_loss, test_acc, p1, p2, p3, p4, p5 = test(epoch)
         
             # save to tsv file
-            detached_train_acc = train_acc.to('cpu').detach().numpy().tolist()
-            detached_train_loss = train_loss.to('cpu').detach().numpy().tolist()
-            detached_test_acc = test_acc.to('cpu').detach().numpy().tolist()
-            detached_test_loss = test_loss.to('cpu').detach().numpy().tolist()
-            update_gspread_ts2(sheet_name, data_name, 'Matsuo-concat', '{}/{}'.format(dataset_path, data_name), detached_train_acc, detached_train_loss, detached_test_acc, detached_test_loss, epoch, epochs, .5, da1, da2, now, otherparams=None)
-
-        if epoch==epochs: # last epoch
-            if not os.path.exists('./data/{}_{}_{}-vs-{}_{}/'.format(sheet_name, data_name, args.da1, args.da2, epoch)):
-                os.makedirs('./data/{}_{}_{}-vs-{}_{}/'.format(sheet_name, data_name, args.da1, args.da2, epoch))
-            torch.save(ts_encoder1.state_dict(), './data/{}_{}_{}-vs-{}_{}/ts_encoder1.pth'.format(sheet_name, data_name, args.da1, args.da2, epoch))
-            torch.save(ts_encoder2.state_dict(), './data/{}_{}_{}-vs-{}_{}/ts_encoder2.pth'.format(sheet_name, data_name, args.da1, args.da2, epoch))
-            torch.save(classifier.state_dict(), './data/{}_{}_{}-vs-{}_{}/classifier.pth'.format(sheet_name, data_name, args.da1, args.da2, epoch))
+            detached_train_acc, detached_train_loss = train_acc.to('cpu').detach().numpy().tolist(), train_loss.to('cpu').detach().numpy().tolist()
+            detached_test_acc, detached_test_loss = test_acc.to('cpu').detach().numpy().tolist(), test_loss.to('cpu').detach().numpy().tolist()
+            detached_p1, detached_p2, detached_p3, detached_p4, detached_p5 = p1, p2, p3, p4, p5
+            update_gspread_ts5(sheet_name, data_name, 'Matsuo-5DA', '{}/{}'.format(dataset_path, data_name), detached_train_acc, detached_train_loss, detached_test_acc, detached_test_loss, epoch, epochs, da1, detached_p1, da2, detached_p2, da3, detached_p3, da4, detached_p4, da5, detached_p5, args.consis_lambda, now, otherparams=None)
+            
+            if epoch==epochs:
+                model_save_path = './data/{}_{}_{}-{}-{}-{}-{}_{}_{}/'.format(sheet_name, data_name, args.da1, args.da2, args.da3, args.da4, args.da5, args.consis_lambda, epoch)
+                if not os.path.exists(model_save_path):
+                    os.makedirs(model_save_path)
+                torch.save(ts_encoder1.state_dict(), model_save_path+'ts_encoder1.pth')
+                torch.save(ts_encoder2.state_dict(), model_save_path+'ts_encoder2.pth')
+                torch.save(ts_encoder3.state_dict(), model_save_path+'ts_encoder3.pth')
+                torch.save(ts_encoder4.state_dict(), model_save_path+'ts_encoder4.pth')
+                torch.save(ts_encoder5.state_dict(), model_save_path+'ts_encoder5.pth')
+                torch.save(classifier.state_dict(), model_save_path+'classifier.pth')
+            
